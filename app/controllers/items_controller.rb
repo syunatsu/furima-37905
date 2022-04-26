7580da7cf85a5bc985379456f486d4f30ba5e0f7
syunatsu <myf.tyys@gmail.com>
2022-04-27 00:09:24 +0900

    itemコントローラー実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,31 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :description, :condition_id, :category_id, :delivery_fee_id, 
+                                 :prefecture_id, :shipment_day_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
