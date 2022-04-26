9031ba945cf618e25f92fa4ee3397ca56002a646
syunatsu <myf.tyys@gmail.com>
2022-04-26 11:26:50 +0900

    gimei(gem)の導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
   
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      
+    end
+    it 'emailが空では登録できない' do
+    end
+    it 'emailが重複すると登録できない' do
+    end
+    it 'emailには@を含まないと登録できない' do
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it 'passwordが6文字以上でないと登録できない' do
+    end
+    it 'passwordは半角英数字混合でないと登録できない' do
+    end
+    it 'passwordとpassword_confirmationが一致しないと登録できない' do
+    end
+    it 'last_nameが空では登録できない' do
+    end
+    it 'first_nameが空では登録できない' do
+    end
+    it 'last_nameは全角で入力しないと登録できない' do
+    end
+    it 'first_nameは全角で入力しないと登録できない' do
+    end
+    it 'last_name_kanaが空では登録できない' do
+    end
+    it 'first_name_kanaが空では登録できない' do
+    end
+    it 'last_name_kanaは全角カタカナで入力しないと登録できない' do
+    end
+    it 'first_name_kanaは全角カタカナで入力しないと登録できない' do
+    end
+    it 'birthdayが空では登録できない' do
+    end
+  end
 end
