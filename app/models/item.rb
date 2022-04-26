0e9c460ee10b1d4e94e415b1acf2416e7c74657c
syunatsu <myf.tyys@gmail.com>
2022-04-26 23:46:23 +0900

    active hashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,15 +3,12 @@ class Item < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999},
             format: {with: /^[0-9]+$/ }
   validates :description, presence: true
-  validates :condition_id, presence: true
-  validates :dategory_id, presence: true
-  validates :delivery_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipment_id, presence: true
+  validates :condition_id, presence: true, numericality: { other_than: 1, message: '---以外を選択してください'}
+  validates :category_id, presence: true, numericality: { other_than: 1, message: '---以外を選択してください'}
+  validates :delivery_fee_id, presence: true, numericality: { other_than: 1, message: '---以外を選択してください'}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: '---以外を選択してください'}
+  validates :shipment_day_id, presence: true, numericality: {other_than: 1, message: '---以外を選択してください'}
   validate :validate_image # 画像のバリデーション
-
-  belongs_to :user
-  has_one_attached :image
 
   private
   # 画像のバリデーションメソッド
@@ -20,4 +17,14 @@ class Item < ApplicationRecord
       errors.add(:imaage, 'の画像を１枚添付してください')
     end
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  belongs_to :category
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :shipment_day
+
+  belongs_to :user
+  has_one_attached :image
 end
