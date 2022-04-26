a42a1643368b77f3f57317975c60fa803d60001a
syunatsu <myf.tyys@gmail.com>
2022-04-26 21:15:03 +0900

    itemモデルのバリデーション実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :condition_id, presence: true
+  validates :dategory_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipment_id, presence: true
+  # validates :image presence: true??
+
 end
