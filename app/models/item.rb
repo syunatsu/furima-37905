47290ac26d1d59bf7147198e2bed41d0964e626e
syunatsu <myf.tyys@gmail.com>
2022-04-26 22:37:26 +0900

    itemモデルにバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999},
+            format: {with: /^[0-9]+$/ }
   validates :description, presence: true
   validates :condition_id, presence: true
   validates :dategory_id, presence: true
