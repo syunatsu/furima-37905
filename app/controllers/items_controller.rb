756a6a1d99e713cea0102f62294c68335fb1286b
syunatsu <myf.tyys@gmail.com>
2022-04-28 19:36:18 +0900

    rubocop実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,7 +39,7 @@ class ItemsController < ApplicationController
     if user_signed_in? && current_user.id == item.user_id
       item.destroy
       redirect_to root_path
-    end 
+    end
   end
 
   private
