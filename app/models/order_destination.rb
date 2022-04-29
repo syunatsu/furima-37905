da7e66d8ac2d5f1a1ead6ce0689911c8e6fce042
syunatsu <myf.tyys@gmail.com>
2022-04-29 20:04:34 +0900

    バリデーションの修正

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,15 +1,15 @@
 class OrderDestination
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :city, :address, :building_name, :order_id, :prefecture_id
+  attr_accessor :user_id, :item_id, :postal_code, :city, :address, :building_name, :phone_number, :order_id, :prefecture_id
     # バリデーションを記述
     with_options presence: true do
-      :user_id
-      :item_id
-      # :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-      :city
-      :address
-      :phone_number
-      # :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+      validates :user_id
+      validates :item_id
+      validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+      validates :city
+      validates :address
+      validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "must be 10 digits or more and 11 digits or less" }
+      validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     end
     #saveメソッドを記述
     def save
